e754b25b600c61573bb93f0e9a2d9461b2a9e5ae
rond <ron.dahan@campus.technion.ac.il>
2020-05-19 18:15:58 +0300

    yeahhh python

---
+++ cmake-build-debug/CMakeFiles/hw2a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2a.dir/main.cpp.o"
+  "hw2a"
+  "hw2a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
